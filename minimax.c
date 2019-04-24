b50cd0d591a4b7ab5f8acaa00c925e7b9584c46f
HITshin <hiteshshikne@gmail.com>
2019-04-24 07:23:28 +0530

    Add files via upload

---
+++ minimax.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<time.h>
+int max, min;
+int a[100];
+void maxmin(int i, int j)
+{
+ 
+ int max1, min1, mid;
+  if(i==j)
+ {
+  max = min = a[i];
+ }
+ else
+ {
+  if(i == j)
+  {
+   if(a[i] <a[j])
+   {
+    max = a[j];
+    min = a[i];
+   }
+   else
+   {
+    max = a[i];
+    min = a[j];
+   } 
+  }
+  else
+  {
+   mid = (i+j)/2;
+   maxmin(i, mid);
+   max1 = max; min1 = min;
+   maxmin(mid+1, j);
+   if(max <max1)
+    max = max1;
+   if(min > min1)
+    min = min1;
+  }
+ }
+}
+
+
+int main ()
+{
+ int i, num;
+  time_t t;
+
+ clock_t begin = clock();
+ printf ("\nEnter the total number of numbers : ");
+ scanf ("%d",&num);
+ printf ("Enter the numbers : \n");
+ for (i=1;i<=num;i++)
+  scanf ("%d",&a[i]);
+
+ max = a[0];
+ min = a[0];
+ maxmin(1, num);
+ printf ("Minimum element in an array : %d\n", min);
+ printf ("Maximum element in an array : %d\n", max);
+ 
+  clock_t end = clock();
+double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("\n execution time is: %f\n",time_spent);
+
+ return 0;
+}
+
